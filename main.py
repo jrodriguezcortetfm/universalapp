6147c18b66d2ba693bf6a3ea2af71a3fc17e3d24
jrodriguezcortetfm <jonathan.ucm.tfm@gmail.com>
2023-08-08 21:32:12 -0400

    Changes

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-import pickle
+from transformers import (
+   AutoTokenizer,
+   TFAutoModelForSequenceClassification,
+)
+import tensorflow as tf
 import sys
+import pandas
+import numpy as np
 import sqlalchemy
 from db import getconn
 
-app = Flask(__name__)
-app.secret_key = "3d6f45a5fc12445dbac2f59c3b6c7cb122" 
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/js/credentials.json"
 
-# Función que se encarga de aplizar TfidfVectorizer sobre el texto 
-# se recibe el texto enviado por el usuario
-# se devuelve la sparse matrix asociada
-def vectorizar(text):
-    # Lectura del vectorizador 
-    vectorizer = pickle.load(open('pickle/tfidf.pkl', 'rb'))
-    # Transformación del texto de entrada
-    print('abrió archivo pickle de vectorización', file=sys.stderr)
-    new_data_vec = vectorizer.transform([text])
-    print('realizó la vectorización', file=sys.stderr)
-    return new_data_vec
+app = Flask(__name__)
+
+app.secret_key = "3d6f45a5fc12445dbac2f59c3b6c7cb134" 
 
 # Función que se encarga de realizar la predicción 
 # se recibe el texto producto de la vectorización
 # se devuelve el resultado del texto si es clasificado como positivo o negativo
-def predict_model(data_vec):
+def predict_model(input_text):
     # Lectura del mejor modelo
-    loaded_model = pickle.load(open('pickle/machine_learning_best_model.pkl', 'rb'))
-    # Predicción
-    prediction = loaded_model.predict(data_vec)
+    tokenizer = AutoTokenizer.from_pretrained("model/tokenizer")
+    model = TFAutoModelForSequenceClassification.from_pretrained("model/modelo")
+    # Tokenizamos esa frase (encode)
+    input_ids = tf.constant(tokenizer.encode(input_text, add_special_tokens=True))[None, :]
+    # Predecimos
+    pred = model.predict(input_ids)["logits"]
+    # Indice del argumento máximo
+    preds = np.argmax(pred,axis=1)
     # Resultado
-    result = "Positivo" if prediction[0] == 1 else print("Negativa")
+    result = "Reseña positiva" if preds[0] == 1 else print("Reseña negativa")
     return result
 
-# Función que permite iniciar sesión al usuario
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -64,41 +62,31 @@ def login():
         return render_template("login.html",error_message = "Usuario o password inválido")
     return render_template("login.html")
 
-# Función que realiza el logout del usuario
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
     session.pop('username', None)    
     return redirect(url_for('login'))
 
-# Muestra el contenido de la página inicial
 @app.route('/index')
 def index():
     return render_template("index.html")
 
-# Muestra el contenido de la sección de Dashboard
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
 
-# Permite ingresar el texto a predecir y obtener el resultado de la predicción
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     print(request.method, file=sys.stderr)
     if request.method == 'POST':
         text = request.form['textpredict']
         print(text, file=sys.stderr)
-        data_vec = vectorizar(text)
-        print('pasó vectorización',file=sys.stderr)
-        predict_result = predict_model(data_vec)
+        predict_result = predict_model(text)
         print(predict_result, file=sys.stderr)
-        if predict_result == "Positivo":
-            final_result = '<h4>La reseña ha sido clasificada como: <b style="color:green">Positiva</b></h4>'
+        if predict_result == "Reseña positiva":
+            final_result = '<h4 >La reseña ha sido clasificada como: <b style="color:green !important">Positiva</b></h4>'
         else:
-            final_result = '<h4>La reseña ha sido clasificada como: <b style="color:red">Negativa</b></h4>'    
-        print('resultados predict:',file=sys.stderr)
-        print(final_result,file=sys.stderr)
-        print(text,file=sys.stderr)
+            final_result = '<h4 >La reseña ha sido clasificada como: <b style="color:red !important">Negativa</b></h4>'    
         return render_template("predict.html", result = final_result, review = text)
     return render_template("predict.html")
-
