fc8938307f46293fff350851be59bf1cf335b5f5
jrodriguezcortetfm <jonathan.ucm.tfm@gmail.com>
2023-08-10 14:40:53 -0400

    Clean code

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from transformers import (
-   AutoTokenizer,
-   TFAutoModelForSequenceClassification,
-)
-import tensorflow as tf
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+import pickle
 import sys
-import pandas
-import numpy as np
 import sqlalchemy
 from db import getconn
 
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/js/credentials.json"
-
 app = Flask(__name__)
 
-app.secret_key = "3d6f45a5fc12445dbac2f59c3b6c7cb134" 
+app.secret_key = "3d6f45a5fc12445dbac2f59c3b6c7cb122" 
+
+# Función que se encarga de aplizar TfidfVectorizer sobre el texto 
+# se recibe el texto enviado por el usuario
+# se devuelve la sparse matrix asociada
+def vectorizar(text):
+    # Lectura del vectorizador 
+    vectorizer = pickle.load(open('pickle/tfidf.pkl', 'rb'))
+    # Transformación del texto de entrada
+    new_data_vec = vectorizer.transform([text])
+    return new_data_vec
 
 # Función que se encarga de realizar la predicción 
 # se recibe el texto producto de la vectorización
 # se devuelve el resultado del texto si es clasificado como positivo o negativo
-def predict_model(input_text):
+def predict_model(data_vec):
     # Lectura del mejor modelo
-    tokenizer = AutoTokenizer.from_pretrained("model/tokenizer")
-    model = TFAutoModelForSequenceClassification.from_pretrained("model/modelo")
-    # Tokenizamos esa frase (encode)
-    input_ids = tf.constant(tokenizer.encode(input_text, add_special_tokens=True))[None, :]
-    # Predecimos
-    pred = model.predict(input_ids)["logits"]
-    # Indice del argumento máximo
-    preds = np.argmax(pred,axis=1)
+    loaded_model = pickle.load(open('pickle/machine_learning_best_model.pkl', 'rb'))
+    # Predicción
+    prediction = loaded_model.predict(data_vec)
     # Resultado
-    result = "Reseña positiva" if preds[0] == 1 else print("Reseña negativa")
+    result = "Reseña positiva" if prediction[0] == 1 else "Reseña negativa"
     return result
 
 @app.route('/')
@@ -39,8 +39,6 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password-field']
-        print(request.form['username'], file=sys.stderr)
-        print(request.form['password-field'], file=sys.stderr)
         pool = sqlalchemy.create_engine(
             "mysql+pymysql://",
             creator=getconn,
@@ -54,7 +52,6 @@ def login():
                 )
             
             result = db_conn.execute(login_query, {'x' : username, 'y' :password}).fetchall()
-            print(result, file=sys.stderr)
         if result:
             session['loggedin'] = True
             session['username'] = username
@@ -78,12 +75,10 @@ def dashboard():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    print(request.method, file=sys.stderr)
     if request.method == 'POST':
         text = request.form['textpredict']
-        print(text, file=sys.stderr)
-        predict_result = predict_model(text)
-        print(predict_result, file=sys.stderr)
+        data_vec = vectorizar(text)
+        predict_result = predict_model(data_vec)
         if predict_result == "Reseña positiva":
             final_result = '<h4 >La reseña ha sido clasificada como: <b style="color:green !important">Positiva</b></h4>'
         else:
